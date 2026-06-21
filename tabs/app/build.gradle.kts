61744a57ed1b39da29b89db90f476552c23fbd2b
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-06-21 22:12:12 +0900

    build: added tabs

---
+++ tabs/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+application {
+    mainClass = "com.mammb.tabs.App"
+    applicationDefaultJvmArgs = listOf(
+        "-XX:+UseCompactObjectHeaders",
+        "--enable-native-access=javafx.graphics", // Restricted methods will be blocked in a future release unless native access is enabled
+    )
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+javafx {
+    version = "25"
+    modules("javafx.controls")
+}
---
+++ tabs/app/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.mammb.tabs {
+    requires javafx.controls;
+    exports com.mammb.tabs;
+}
