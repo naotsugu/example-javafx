0376469b2b31282b0d4a6e013f24bcd881802b7a
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-07-17 16:21:17 +0900

    build: mod

--- multi-tabs/app/src/main/java/com/mammb/javafx/pane/tabblock/internal/TabButton.java
+++ multi-tabs/app/src/main/java/com/mammb/javafx/pane/tabblock/internal/TabButton.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2026- the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mammb.javafx.pane.tabblock.internal;
 
 import javafx.scene.control.Label;
@@ -59,7 +74,7 @@ class TabButton extends javafx.scene.control.Tab {
                 -fx-text-fill: transparent;
             }
             .add-tab-button-label {
-              -fx-padding: 3 5 3 5;
+              -fx-padding: 2 5 2 5;
               -fx-background-radius: 15px;
               -fx-border-radius: 15px;
             }
