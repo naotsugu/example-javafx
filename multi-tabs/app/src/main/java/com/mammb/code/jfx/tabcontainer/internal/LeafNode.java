73d00435b1af45499b7322ef0791e7289193940e
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2026-08-26 16:50:35 +0900

    refactor: TabContainer

--- multi-tabs/app/src/main/java/com/mammb/code/jfx/tabcontainer/internal/LeafNode.java
+++ multi-tabs/app/src/main/java/com/mammb/code/jfx/tabcontainer/internal/LeafNode.java
@@ -177,11 +177,11 @@ public class LeafNode extends TreeNode implements ParentOf<Tab> {
         if (e.getDragboard().hasFiles() && dragOnTabHeader) {
             List<Path> paths = db.getFiles().stream()
                 .filter(File::exists).filter(File::canRead).map(File::toPath).toList();
-            paths.forEach(path -> ctx.handlers().requestContent(path));
             if (paths.isEmpty()) {
                 e.setDropCompleted(true);
                 e.consume();
             }
+            paths.forEach(path -> ctx.handlers().requestContent(path));
             return;
         }
 
